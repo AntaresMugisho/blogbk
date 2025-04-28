d7788198c9a53d4c6d9e111f9ced5109c77bdf90
Antares Mugisho <antaresmugisho@gmail.com>
2025-04-28 07:15:12 -0400

    Make migrations and add EditorJS upload image link

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
+from .views import UploadFileView
 
 urlpatterns = [
     path('auth/', include("apps.accounts.urls")),
     path('blog/', include("apps.blog.urls")),
+    path("editorjs/upload", UploadFileView.as_view())
 ]

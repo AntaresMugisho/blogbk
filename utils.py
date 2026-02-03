aec9ef76ddf160c36020de8e050d3cd2b374913b
AntaresMugisho <antaresmugisho@gmail.com>
2026-02-03 21:44:09 +0200

    Pluralize folder names for uploaded files

--- utils.py
+++ utils.py
@@ -4,4 +4,4 @@ import uuid
 def random_filename(instance, filename):
     extension = os.path.splitext(filename)[1]
     filename = f"{uuid.uuid4().hex}{extension}"
-    return os.path.join(f"{instance.__class__.__name__.lower()}", filename)
+    return os.path.join(f"{instance.__class__.__name__.lower()}s", filename)

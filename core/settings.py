c634858a252c274e7df078fb80d584c974cd8968
AntaresMugisho <antaresmugisho@gmail.com>
2026-02-05 12:28:18 +0200

    Change media url back to files

--- core/settings.py
+++ core/settings.py
@@ -141,7 +141,7 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = "static/"
 
-MEDIA_URL = "media/"
+MEDIA_URL = "files/"
 
 MEDIA_ROOT = "files/"
 
@@ -149,6 +149,7 @@ MEDIA_ROOT = "files/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
